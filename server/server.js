0f36c5dde8c202e9ceef234717ba177c5fbe82dc
isuriranasinghe20 <it23838970@my.sliit.lk>
2026-05-06 19:46:40 +0530

    Added deployment config

--- server/server.js
+++ server/server.js
@@ -7,6 +7,16 @@ const connectDB = require("./config/db");
 const app = express();
 connectDB();
 
+const path = require("path");
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../client/build")));
+
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../client/build/index.html"));
+  });
+}
+
 app.use(cors());
 app.use(express.json());
 
