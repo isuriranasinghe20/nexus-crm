73ca0c730c7e45544390fa25bdbb6e65f491d434
isuriranasinghe20 <it23838970@my.sliit.lk>
2026-05-06 23:03:43 +0530

    Fix catch-all route syntax for SPA routing

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use("/api/notes", require("./routes/notes"));
 app.use(express.static(path.join(__dirname,"../client/build")));
 
 // Fallback to index.html for all non-API routes (SPA routing)
-app.get("*", (req,res)=>{
+app.get(/.*/, (req,res)=>{
   res.sendFile(path.join(__dirname,"../client/build/index.html"));
 });
 
